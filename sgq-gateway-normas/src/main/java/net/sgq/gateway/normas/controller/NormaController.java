a68d8498e48221b622e4f8d7777f61b4799eb4f7
Diogo Silvério <dblsilverio+github@gmail.com>
2020-04-19 21:57:48 -0300

    Gateway de normas com documentação

--- sgq-gateway-normas/src/main/java/net/sgq/gateway/normas/controller/NormaController.java
+++ sgq-gateway-normas/src/main/java/net/sgq/gateway/normas/controller/NormaController.java
@@ -4,12 +4,23 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import net.sgq.gateway.normas.modelos.Norma;
 
 public interface NormaController {
 
+	@ApiOperation(value = "Busca no módulo externo de Gestão de Normas todas as normas que estiverem disponíveis", response = Norma.class, responseContainer = "List")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Lista de normas"),
+			@ApiResponse(code = 404, message = "Nenhuma norma encontrada"),
+			@ApiResponse(code = 502, message = "Sistema fora do ar") })
 	ResponseEntity<List<Norma>> listaNormas();
 
+	@ApiOperation(value = "Busca no módulo externo de Gestão de Normas uma norma por seu identificador", response = Norma.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Norma retornada com sucesso"),
+			@ApiResponse(code = 404, message = "Nenhuma norma encontrada"),
+			@ApiResponse(code = 502, message = "Sistema fora do ar") })
 	ResponseEntity<Norma> consultaNorma(Long id);
 
 }
