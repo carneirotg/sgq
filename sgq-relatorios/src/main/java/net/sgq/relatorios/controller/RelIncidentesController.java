0d0aabb19a36d40b5f226ec176f73d5028a081b9
Diogo Silvério <dblsilverio+github@gmail.com>
2020-04-28 21:22:26 -0300

    Inclusão de documentaçao da API de relatórios

--- sgq-relatorios/src/main/java/net/sgq/relatorios/controller/RelIncidentesController.java
+++ sgq-relatorios/src/main/java/net/sgq/relatorios/controller/RelIncidentesController.java
@@ -4,16 +4,35 @@ import java.util.Date;
 
 import org.springframework.http.ResponseEntity;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 public interface RelIncidentesController {
 
+	@ApiOperation(value = "Relatório de incidentes do mês atual", response = byte[].class, responseContainer = "Pdf")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Relatório gerado com sucesso") })
 	ResponseEntity<byte[]> incidentesMesAtual();
 	
+	@ApiOperation(value = "Relatório de incidentes dos últimos seis meses", response = byte[].class, responseContainer = "Pdf")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Relatório gerado com sucesso") })
 	ResponseEntity<byte[]> incidentesSemestre();
 	
+	@ApiOperation(value = "Relatório de incidentes do ano atual", response = byte[].class, responseContainer = "Pdf")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Relatório gerado com sucesso") })
 	ResponseEntity<byte[]> incidentesAnoCorrente();
 	
+	@ApiOperation(value = "Relatório de incidentes dos últimos doze meses", response = byte[].class, responseContainer = "Pdf")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Relatório gerado com sucesso") })
 	ResponseEntity<byte[]> incidentesUltimosDozeMeses();
 	
+	@ApiOperation(value = "Relatório de incidentes do período apontado", response = byte[].class, responseContainer = "Pdf")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Relatório gerado com sucesso") })
 	ResponseEntity<byte[]> incidentesPorPeriodo(Date inicio, Date fim);
 	
 }
