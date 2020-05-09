b42117dd3ea8da6351c055f45068d4594f75d5f9
Diogo Silvério <dblsilverio+github@gmail.com>
2020-05-08 22:38:31 -0300

    Cache noop para testes

---
+++ sgq-gateway-normas/src/test/java/net/sgq/gateway/normas/config/CacheConfig.java
@@ -0,0 +1,16 @@
+package net.sgq.gateway.normas.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.support.NoOpCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CacheConfig {
+
+	@Bean
+	public CacheManager cacheManager() {
+		return new NoOpCacheManager();
+	}
+	
+}
