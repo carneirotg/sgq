f6dd1499db4c409e027952b5f2a67beb4d8c1497
Diogo Silvério <dblsilverio+github@gmail.com>
2020-05-16 16:57:24 -0300

    Abrindo rota publica para eventos

--- sgq-transparencia/src/main/java/net/sgq/transparencia/config/SecurityConfig.java
+++ sgq-transparencia/src/main/java/net/sgq/transparencia/config/SecurityConfig.java
@@ -4,21 +4,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfig extends ResourceServerConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter /*ResourceServerConfigurerAdapter*/ {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/**").fullyAuthenticated()
-				.antMatchers(HttpMethod.GET, "/**/eventos/**").permitAll()
+			.antMatchers("/**").authenticated()
 				.and().cors()
 				.and().csrf().disable()
 				.formLogin().disable()
 				.httpBasic().disable()
 				.anonymous().disable();
-	}}
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(HttpMethod.GET, "/eventos");
+	}
+
+
+}
